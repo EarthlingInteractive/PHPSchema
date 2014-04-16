c24ada6d82d27f462de283a4ee1615847c43b837
Dan Stevens <stevens@earthit.com>
2014-04-16 17:46:12 -0500

    Add database namespace path field to ResourceClass.

--- lib/EarthIT/Schema/ResourceClass.php
+++ lib/EarthIT/Schema/ResourceClass.php
@@ -8,6 +8,18 @@ class EarthIT_Schema_ResourceClass extends EarthIT_Schema_SchemaObject
 	protected $membersAreMutable = false;
 	protected $membersSetIsMutable = false;
 	protected $tableNameOverride = null;
+	/**
+	 * An array of namespace components under which the table corresponding
+	 * to this class can be found.
+	 * If the database/schema will be implied by the connection, this will be an empty array.
+	 * If a schema needs to be specified per-table to do queries on it, this would
+	 * contain the name of the schema.
+	 * If neither the database name nor the schema are implied, it would contain both.
+	 * e.g. array('some_database','some_schema')
+	 * A table named "some_table" would then be referred to as
+	 * "some_database"."some_schema"."some_table" in SQL queries.
+	 */
+	protected $dbNamespacePath = array();
 	
 	protected $fields = array();
 	protected $references = array();
@@ -18,6 +30,7 @@ class EarthIT_Schema_ResourceClass extends EarthIT_Schema_SchemaObject
 	public function membersArePublic() { return $this->membersArePublic; }
 	public function membersAreMutable() { return $this->membersAreMutable; }
 	public function membersSetIsMutable() { return $this->memberSetIsMutable; }
+	public function getDbNamespacePath() { return $this->dbNamespacePath; }
 	public function getTableNameOverride() { return $this->tableNameOverride; }
 	
 	public function getFields() { return $this->fields; }
