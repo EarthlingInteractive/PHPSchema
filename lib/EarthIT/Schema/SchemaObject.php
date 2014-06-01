383c5eda955edff3e6a9a6b005ddfabca4ccc440
Dan Stevens <stevens@earthit.com>
2014-06-01 14:16:05 -0500

    Add SchemaObject#properties and accessor methods.

--- lib/EarthIT/Schema/SchemaObject.php
+++ lib/EarthIT/Schema/SchemaObject.php
@@ -3,6 +3,11 @@
 class EarthIT_Schema_SchemaObject
 {
 	protected $name;
+	/**
+	 * Generic array of properties.
+	 * array( fully namespaced property name => array( property values... ) )
+	 */
+	protected $properties = array();
 	
 	public function getName() { return $this->name; }
 
@@ -12,5 +17,13 @@ class EarthIT_Schema_SchemaObject
 			$obj->$k = $v;
 		}
 		return $obj;
+	}
+	
+	public function getProperties() {
+		return $this->properties;
+	}
+	public function getFirstPropertyValue( $longName ) {
+		if( isset($this->properties[$longName]) ) foreach($this->properties[$longName] as $v) return $v;
+		return null;
 	}
 }
