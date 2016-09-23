4c4cfc0b9e7a91e537e7a73bc3f6d51f0ba13e24
Dan Stevens <stevens@earthit.com>
2016-09-23 13:06:46 -0500

    getResourceClass will now find by entity ID if finding by name fails.

--- lib/EarthIT/Schema.php
+++ lib/EarthIT/Schema.php
@@ -5,15 +5,20 @@ class EarthIT_Schema
 	protected $resourceClasses;
 	protected $resourceClassesByMinName;
 	protected $resourceClassesByLongName;
+	protected $resourceClassesByEntityId;
 	
 	public function __wakeup() {
 		$this->resourceClassesByMinName = array();
 		$this->resourceClassesByLongName = array();
+		$this->resourceClassesByEntityId = array();
 		foreach( $this->resourceClasses as $rc ) {
 			$min = EarthIT_Schema_WordUtil::minimize($rc->getName());
 			$this->resourceClassesByMinName[$min] = $rc;
 			if( ($longName = $rc->getLongName()) !== null ) {
 				$this->resourceClassesByLongName[$longName] = $rc;
+			}
+			foreach( $rc->getPropertyValues("http://ns.nuke24.net/Phrebar/entityId") as $entityId ) {
+				$this->resourceClassesByEntityId[$entityId] = $rc;
 			}
 		}
 	}
@@ -29,6 +34,9 @@ class EarthIT_Schema
 		$min = EarthIT_Schema_WordUtil::minimize($name);
 		if( isset($this->resourceClassesByMinName[$min]) ) {
 			return $this->resourceClassesByMinName[$min];
+		}
+		if( isset($this->resourceClassesByEntityId[$name]) ) {
+			return $this->resourceClassesByEntityId[$name];
 		}
 		throw new EarthIT_Schema_NoSuchResourceClass($name);
 	}
